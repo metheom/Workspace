46b5f19ca8bd4622f657d3269cecd61f07290a37
metheom <merttaser@gmail.com>
2022-11-06 10:08:12 +0300

    Update!

---
+++ Ayberk/Dersler/Ders-1/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // ClassName objName = new ClassName();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Bir sayı girin: ");
+//        int num1 = scanner.nextInt();
+//
+//        System.out.print("Bir sayı gir: ");
+//        int num2 = scanner.nextInt();
+//
+//        if (num1 > num2) {
+//            System.out.println("Numara 1 buyuk");
+//        } else if (num1 == num2) {
+//            System.out.println("Numara 1 esittir numara 2");
+//        } else {
+//            System.out.println("Numara 2 buyuk");
+//        }
+
+//        for (int i = 1; i <= 20; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+
+        /*
+                                    ASSIGNMENT
+
+                Let user to enter a string and then check if that string is palindrome or not.
+
+                        ADA => True
+                        Mert => False
+
+                Palindrome: If string is equal to its reversed order its an palindrome.
+
+
+         */
+
+        System.out.println("Hello World" +);
+
+
+    }
+}
